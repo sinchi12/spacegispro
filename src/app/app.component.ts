a59fcd100939f2f19ec1f35aaa52237a037a7374
sinchi12 <leslie-alexander97@hotmail.es>
2020-06-26 15:40:15 -0500

    funcional los puntos de referencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,24 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
 
-    Mapboxgl.accessToken = environment.mapboxKey;
+    (Mapboxgl as any).accessToken = environment.mapboxKey;
 
     this.mapa = new Mapboxgl.Map({
       container: 'mapa-mapbox',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-79.4697205, -1.0197277],
-      zoom: 14.33
+      center: [-79.5001181, -1.0196141],
+      zoom: 13
     });
+    this.crearMarcador(-79.4687127, -1.0134393);
+  }
+
+  crearMarcador(lng: number, lat: number) {
+    const marker = new Mapboxgl.Marker({
+      draggable: true
+    }).setLngLat([lng, lat])
+    .addTo(this.mapa);
+
+    marker.on('drag', () => {console.log(marker.getLngLat())});
   }
 
 }
